9702a8cfad7414fbf68f1f1173ca5cdbc62c7374
Vivek Ramaswamy <ramaswamy.vivek@gene.com>
2015-09-20 06:56:51 -0700

    looping a single image so it fits into a framerate of 29.97 fps for a single second

--- images/mov.bash
+++ images/mov.bash
@@ -1,14 +1,16 @@
 #!/bin/bash 
-declare -a STORY_ELEMS=("OnceUponATime" "AndEveryDay" "ThenOneDay.jpg" "BecauseOfThat" "UntilFinally" "AndEverSinceThen")
-IPHONE_WIDTH=960
-IPHONE_HEIGHT=720
+declare -a STORY_ELEMS=("OnceUponATime" "AndEveryDay" "ThenOneDay" "BecauseOfThat" "UntilFinally" "AndEverSinceThen")
+#1280x720
+IPHONE_WIDTH=720
+IPHONE_HEIGHT=1280
 IPHONE_EXT=".mov"
-
+IPHONE_ASPECT_RATIO="16:9"
+IPHONE_PIXEL_FORMAT="yuv420p"
 ANDROID_WIDTH=704
 ANDROID_HEIGHT=576
 ANDROID_EXT=".3gp"
 
-FRAMERATE=1
+FRAMERATE=29.97
 
 
 echo "rm ./*.mov ./*.3gp"
@@ -20,7 +22,7 @@ do
    iphone_int_mov=$i"_int"$IPHONE_EXT
    iphone_mov=$i$IPHONE_EXT
    iphone_dim=$IPHONE_WIDTH"x"$IPHONE_HEIGHT
-   iphone_int_cmd="ffmpeg -pattern_type glob -framerate $FRAMERATE -y -i ./$jpg -s $iphone_dim $iphone_int_mov"  
+   iphone_int_cmd="ffmpeg -pattern_type glob -loop 1 -t 1 -framerate $FRAMERATE -y -i ./$jpg -s $iphone_dim -pix_fmt $IPHONE_PIXEL_FORMAT  -vf \"setsar=sar=16:9,setdar=dar=1:1\" $iphone_int_mov" 
    iphone_cmd="ffmpeg -i ./$iphone_int_mov -i ./1secAudio.mp3 -map 0:0 -map 1:0 $iphone_mov"
    iphone_rm_int_cmd="rm ./$iphone_int_mov"
    echo $iphone_int_cmd
