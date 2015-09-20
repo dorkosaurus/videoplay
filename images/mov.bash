26c839cd8de14f94aa7501ef0d07afe3325495e1
Vivek Ramaswamy <ramaswamy.vivek@gene.com>
2015-09-20 03:47:38 -0700

    added audio to iphone image movies so concatenation will work as expected

--- images/mov.bash
+++ images/mov.bash
@@ -13,10 +13,16 @@ FRAMERATE=1
 for i in "${STORY_ELEMS[@]}"
 do
    jpg=$i".jpg"
+   iphone_int_mov=$i"_int"$IPHONE_EXT
    iphone_mov=$i$IPHONE_EXT
+   
    iphone_dim=$IPHONE_WIDTH"x"$IPHONE_HEIGHT
-   iphone_cmd="ffmpeg -pattern_type glob -framerate $FRAMERATE -y -i '$jpg' -s $iphone_dim $iphone_mov"    
+   iphone_int_cmd="ffmpeg -pattern_type glob -framerate $FRAMERATE -y -i '$jpg' -s $iphone_dim $iphone_int_mov"  
+   iphone_cmd="ffmpeg -i ./$iphone_int_mov -i ./1secAudio.mp3 -map 0:0 -map 1:0 $iphone_mov"
+   iphone_rm_int_cmd="rm ./$iphone_int_mov"
+   echo $iphone_int_cmd
    echo $iphone_cmd
+   echo $iphone_rm_int_cmd
 
    android_mov=$i$ANDROID_EXT
    android_dim=$ANDROID_WIDTH"x"$ANDROID_HEIGHT
