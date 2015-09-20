c0f6c009a713b12655ddcf837416b9aed68678b5
Vivek Ramaswamy <ramaswamy.vivek@gene.com>
2015-09-20 04:06:38 -0700

    hand script to generate iphone and android video/audio from images

--- images/mov.bash
+++ images/mov.bash
@@ -10,14 +10,17 @@ ANDROID_EXT=".3gp"
 
 FRAMERATE=1
 
+
+echo "rm ./*.mov ./*.3gp"
+
 for i in "${STORY_ELEMS[@]}"
 do
    jpg=$i".jpg"
+
    iphone_int_mov=$i"_int"$IPHONE_EXT
    iphone_mov=$i$IPHONE_EXT
-   
    iphone_dim=$IPHONE_WIDTH"x"$IPHONE_HEIGHT
-   iphone_int_cmd="ffmpeg -pattern_type glob -framerate $FRAMERATE -y -i '$jpg' -s $iphone_dim $iphone_int_mov"  
+   iphone_int_cmd="ffmpeg -pattern_type glob -framerate $FRAMERATE -y -i ./$jpg -s $iphone_dim $iphone_int_mov"  
    iphone_cmd="ffmpeg -i ./$iphone_int_mov -i ./1secAudio.mp3 -map 0:0 -map 1:0 $iphone_mov"
    iphone_rm_int_cmd="rm ./$iphone_int_mov"
    echo $iphone_int_cmd
@@ -25,9 +28,14 @@ do
    echo $iphone_rm_int_cmd
 
    android_mov=$i$ANDROID_EXT
+   android_int_mov=$i"_int"$ANDROID_EXT
    android_dim=$ANDROID_WIDTH"x"$ANDROID_HEIGHT
-   android_cmd="ffmpeg -pattern_type glob -framerate $FRAMERATE -y -i '$jpg' -s $android_dim $android_mov"
+   android_int_cmd="ffmpeg -pattern_type glob -framerate $FRAMERATE -y -i ./$jpg -s $android_dim $android_int_mov"
+   android_cmd="ffmpeg -i ./$android_int_mov -i ./1secAudio.mp3 -map 0:0 -map 1:0 $android_mov"
+   android_rm_int_cmd="rm ./$android_int_mov"
+   echo $android_int_cmd 
    echo $android_cmd
+   echo $android_rm_int_cmd
 
 done
 
